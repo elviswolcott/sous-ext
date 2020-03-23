5cfb468fb5a4e564e43b8f8aa3dd301a9902a804
Elvis <elviswolcott@gmail.com>
2020-03-23 03:51:23 -0500

    feat: detect sites using WordPress Recipe Maker

--- src/recipes/index.js
+++ src/recipes/index.js
@@ -3,7 +3,6 @@ import wprm  from './wprm';
 import Detector from './detector';
 
 const detector = new Detector();
-//detector.register(wprm);
 
 [wprm].forEach(m => { detector.register(m) });
 
---
+++ src/recipes/wprm/index.js
@@ -0,0 +1,11 @@
+/* system: WordPress Recipe Maker
+ * about page: https://wordpress.org/plugins/wp-recipe-maker/
+ * example page: https://www.liveeatlearn.com/mushroom-pulled-pork/
+*/
+
+const config = {
+  name: "wprm",
+  classes: ["wprm-recipe-container"],
+};
+
+export default config;
